751cca7d69f730ffaaf831261f730575d535e927
fahledu <fahledu@gmail.com>
2025-08-20 22:59:36 -0300

    ref: refatorado criar nova nota

--- src/controllers/notes/show.php
+++ src/controllers/notes/show.php
@@ -7,31 +7,13 @@ $db = new Database($config['database']);
 
 $currentUserId = 1;
 
+$heading = 'Note';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->findOrFail();
+$note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->findOrFail();
 
-    authorize($note['user_id'] === $currentUserId);
-    // 
-    $db->query('delete from notes where id = :id', [
-        'id' => $_GET['id']
-    ]);
+authorize($note['user_id'] === $currentUserId);
 
-    header('location: /notes');
-
-    exit();
-
-} else {
-
-    $heading = 'Note';
-
-
-    $note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->findOrFail();
-
-    authorize($note['user_id'] === $currentUserId);
-
-    view('notes/show.view.php', [
-        'heading' => 'Note',
-        'note' => $note
-    ]);
-}
+view('notes/show.view.php', [
+    'heading' => 'Note',
+    'note' => $note
+]);
