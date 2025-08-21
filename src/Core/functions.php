2d7975f269ae3ea76823703cd045f3047b099b28
fahledu <fahledu@gmail.com>
2025-08-20 21:44:36 -0300

    ref: refatorado rotas

--- src/Core/functions.php
+++ src/Core/functions.php
@@ -15,6 +15,12 @@ function urlIs($value)
     return $_SERVER['REQUEST_URI'] === $value;
 }
 
+function abort($code = 404)
+{
+    http_response_code($code);
+    require base_path("views/{$code}.php");
+    die();
+}
 
 function authorize($condition, $status = Response::FORBIDDEN)
 {
@@ -23,11 +29,13 @@ function authorize($condition, $status = Response::FORBIDDEN)
     }
 }
 
-function base_path($path){
+function base_path($path)
+{
     return BASE_PATH . $path;
 }
 
-function view($path, $attributes = []){
+function view($path, $attributes = [])
+{
 
     extract($attributes);
     require BASE_PATH('views/' . $path);
--- src/controllers/notes/show.php
+++ src/controllers/notes/show.php
@@ -5,14 +5,33 @@ use Core\Database;
 $config = require base_path("config.php");
 $db = new Database($config['database']);
 
-$heading = 'Note';
 $currentUserId = 1;
 
-$note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->findOrFail();
 
-authorize($note['user_id'] === $currentUserId);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->findOrFail();
 
-view('notes/show.view.php', [
-    'heading' => 'Note',
-    'note' => $note
-]);
+    authorize($note['user_id'] === $currentUserId);
+    // 
+    $db->query('delete from notes where id = :id', [
+        'id' => $_GET['id']
+    ]);
+
+    header('location: /notes');
+
+    exit();
+
+} else {
+
+    $heading = 'Note';
+
+
+    $note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
+
+    view('notes/show.view.php', [
+        'heading' => 'Note',
+        'note' => $note
+    ]);
+}
