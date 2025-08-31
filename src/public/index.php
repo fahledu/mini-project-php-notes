7f9ec4be9b60835ab699b49add71908570f6ba2f
fahledu <fahledu@gmail.com>
2025-08-31 14:31:34 -0300

    ref: refatorado a validação do form

--- src/public/index.php
+++ src/public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Core\Session;
+use Core\ValidationException;
 
 session_start();
 
@@ -13,7 +14,7 @@ spl_autoload_register(
 
         $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
 
-       require base_path("{$class}.php");
+        require base_path("{$class}.php");
     }
 );
 
@@ -26,6 +27,15 @@ $routes = require base_path('routes.php');
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
 
-$router->route($uri, $method);
+try {
+    $router->route($uri, $method);
+} catch (ValidationException $exception) {
+    Session::flash('errors', $exception->errors);
+    Session::flash('old', $exception->old);
+
+
+    return redirect($router->previousUrl());
+}
+
 
 Session::unFlash();
