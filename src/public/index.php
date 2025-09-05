d9acf23a5e520d59e6016c5b14aa4b17e0886914
fahledu <fahledu@gmail.com>
2025-09-04 21:59:09 -0300

    chore: install composer and ref: to compose auto load

--- src/public/index.php
+++ src/public/index.php
@@ -3,20 +3,13 @@
 use Core\Session;
 use Core\ValidationException;
 
-session_start();
-
 const BASE_PATH = __DIR__ . "/../";
 
 require BASE_PATH . "Core/functions.php";
 
-spl_autoload_register(
-    function ($class) {
+require BASE_PATH . 'vendor/autoload.php';
 
-        $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-
-        require base_path("{$class}.php");
-    }
-);
+session_start();
 
 require base_path('bootstrap.php');
 
